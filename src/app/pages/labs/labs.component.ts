8a134c6518f8a3a06d3721f6229a8119302f793d
Iván González <cafeteru.dev@gmail.com>
2023-12-01 13:19:18 +0100

    Delete package-lock.json

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -16,4 +16,12 @@ export class LabsComponent {
     'Crear un componente',
     'Generar un servicio',
   ];
+
+  name = 'Iván';
+  age = 32;
+  img = 'https://picsum.photos/200/300';
+
+  clickHandler() {
+    alert('Hola');
+  }
 }
